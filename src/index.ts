1b5e1f7d4615f6ff82a8a55e6ef42c0d23cb0100
Boris Zhao <boris1993@live.cn>
2023-11-15 22:05:44 +0800

    Fix

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
+import * as url from 'url';
 import { Ai } from '@cloudflare/ai';
+import { AiTextToImageInput } from '@cloudflare/ai/dist/tasks/text-to-image';
 
 export interface Env {
     AI: any;
 }
 
 export default {
-    async fetch(
-        request: Request,
-        env: Env
-    ): Promise<Response> {
+    async fetch(request: Request, env: Env) {
+        const urlParts = url.parse(request.url);
+        let path = urlParts.pathname;
+        let pathParts = path?.split("/");
+        if (pathParts[1] === "favicon.ico") {
+            return new Response();
+        }
+
         const ai = new Ai(env.AI);
-        const inputs = {
-            prompt: extractPromptFromQueryParams(request)
+        const [prompt, numSteps] = extractPromptFromQueryParams(request);
+        const inputs: AiTextToImageInput = {
+            prompt: prompt,
+            num_steps: numSteps
         };
 
         const response = await ai.run("@cf/stabilityai/stable-diffusion-xl-base-1.0", inputs);
@@ -24,13 +32,19 @@ export default {
     }
 }
 
-function extractPromptFromQueryParams(request: Request): string {
-    const urlParams = new URLSearchParams(request.url);
+function extractPromptFromQueryParams(request: Request): [string, number] {
+    const url = new URL(request.url);
+    const urlParams = url.searchParams;
+
     const prompt = urlParams.get('prompt');
     if (prompt == null) {
         throw new Error("Prompt cannot be empty");
-        
     }
 
-    return prompt;
+    let numSteps: number = Number(urlParams.get('num_steps'));
+    if (!numSteps) {
+        numSteps = 20;
+    }
+
+    return [prompt, numSteps];
 }
